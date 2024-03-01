5281f68ba33242bf3e8eaf9a579792cf65ac06f0
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-03-01 15:01:00 +0200

    added navigationData file to get all navigation links

---
+++ src/data/navigationData.ts
@@ -0,0 +1,8 @@
+const navLinks = [
+  { href: "#home", text: "home" },
+  { href: "#doctors", text: "doctors" },
+  { href: "#services", text: "services" },
+  { href: "#about", text: "about" },
+];
+
+export { navLinks }
