0ddc073e4e44f618ee0055dab32f2ccb2831a3a4
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-03-01 14:58:06 +0200

    added loading component

---
+++ src/components/ui/Loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading: React.FC = () => {
+  return (
+    <div className="mt-5">
+      <span className="loading loading-spinner loading-lg text-center block"></span>
+    </div>
+  );
+}
+
+export default Loading
