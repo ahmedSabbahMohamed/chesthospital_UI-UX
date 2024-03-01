681e8ecbc0b6de90ec22055b1b5d5e22ea5f9810
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-03-01 14:54:58 +0200

    added data file for static data and added services array

---
+++ src/services/data.ts
@@ -0,0 +1,22 @@
+const services = [
+  {
+    id: 1,
+    icon: "assets/images/bed.svg",
+    title: "Bed facility",
+    desc: "Welcome to Medilife,Where exceptional medical care meets compassionate expertise",
+  },
+  {
+    id: 2,
+    icon: "assets/images/medicine.svg",
+    title: "Medicines",
+    desc: "Welcome to Medilife,Where exceptional medical care meets compassionate expertise",
+  },
+  {
+    id: 3,
+    icon: "assets/images/care.svg",
+    title: "Total Care",
+    desc: "Welcome to Medilife,Where exceptional medical care meets compassionate expertise",
+  },
+];
+
+export { services }
