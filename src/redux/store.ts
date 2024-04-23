84fec606787f4b4950acf16e8ce27870cb67da22
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-04-23 14:37:40 +0200

    added patientId reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import patientId from "./slices/patientSlice";
 
 const store = configureStore({
   reducer: {
-    
+    id: patientId,
   },
 });
 
