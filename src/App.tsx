04520e2054221b27154724bf257d919656379a12
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-04-23 14:28:50 +0200

    added ReceptionRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import { Route, Routes } from "react-router-dom"
-import LandingPage from "./pages/LandingPage"
-import Login from "./pages/Login"
 import { useTranslation } from "react-i18next"
+import ReceptionRoutes from "./routes/ReceptionRoutes"
 
 function App() {
   const { t } = useTranslation()
 
   return (
     <div dir={t("dir")}>
-      <Routes>
-        <Route path={"/"} element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <ReceptionRoutes />
     </div>
   )
 }
