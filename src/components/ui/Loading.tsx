c23c9f921f9f8945714e74d0fc2c56929686c406
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-04-23 14:31:55 +0200

    removed margint top class

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loading: React.FC = () => {
   return (
-    <div className="mt-5">
+    <div>
       <span className="loading loading-spinner loading-lg text-center block"></span>
     </div>
   );
