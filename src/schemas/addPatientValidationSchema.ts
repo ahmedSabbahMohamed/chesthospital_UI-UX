72d75f9ae6a024609952a321b7e91b211819564f
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-05-18 18:48:34 +0300

    updated the length of the id to match the backend constaraints

--- src/schemas/addPatientValidationSchema.ts
+++ src/schemas/addPatientValidationSchema.ts
@@ -1,32 +1,27 @@
 import * as Yup from "yup";
 
 const initialValues = {
-    id: '',
-    name: '',
-    email: '',
-    phone: '',
-    address: '',
-    dateOfBirth: '',
+  id: "",
+  name: "",
+  email: "",
+  phone: "",
+  address: "",
+  dateOfBirth: "",
 };
 
 const validationSchema = Yup.object().shape({
   id: Yup.string()
-    .matches(/^\d{14}$/, "Please enter exactly 14 numbers")
+    .matches(/^\d{10}$/, "Please enter exactly 10 numbers")
     .required("id is required"),
-  name: Yup.string()
-    .required("الإسم الكامل ضروري")
-    .min(15, "الإسم قصير جدا"),
+  name: Yup.string().required("الإسم الكامل ضروري").min(15, "الإسم قصير جدا"),
   email: Yup.string()
     .email("invalid email address")
     .required("عنوان البريد الإلكتروني مطلوب"),
   phone: Yup.string()
     .matches(/^01\d{9}$/, "صيغة خاطئة")
     .required("الهاتف مطلوب"),
-  address: Yup.string()
-    .min(20, "العنوان قصير جدًا")
-    .required("العنوان مطلوب"),
-    dateOfBirth: Yup.string()
-      .required("تاريخ الميلاد مطلوب"),
+  address: Yup.string().min(20, "العنوان قصير جدًا").required("العنوان مطلوب"),
+  dateOfBirth: Yup.string().required("تاريخ الميلاد مطلوب"),
 });
 
-export { initialValues, validationSchema }
+export { initialValues, validationSchema };
--- src/schemas/searchPatientValidationSchema.ts
+++ src/schemas/searchPatientValidationSchema.ts
@@ -1,13 +1,13 @@
 import * as Yup from "yup";
 
 const initialValues = {
-    id: ""
-}
+  id: "",
+};
 
 const searchPatientSchema = Yup.object({
   id: Yup.string()
-    .matches(/^\d{14}$/, "Please enter exactly 14 numbers")
+    .matches(/^\d{10}$/, "Please enter exactly 10 numbers")
     .required("id is required"),
 });
 
-export { initialValues, searchPatientSchema }
+export { initialValues, searchPatientSchema };
