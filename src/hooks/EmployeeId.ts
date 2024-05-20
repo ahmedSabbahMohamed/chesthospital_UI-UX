9656aadd4c9440176f898903a580ad8f309941db
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-05-20 05:50:36 +0300

    created custom hook to get employee id from redux

---
+++ src/hooks/EmployeeId.ts
@@ -0,0 +1,9 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
+
+const useEmployeeId = () => {
+    const { employeeId } = useSelector((state: RootState) => state);
+    return employeeId;
+};
+
+export default useEmployeeId;
