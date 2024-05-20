d560e4af28c7407471926da0e5720b3c7802d2bf
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-05-20 05:57:14 +0300

    added token to headers

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -2,8 +2,30 @@ import axios from "axios";
 
 const BASI_API = `http://localhost:5000`;
 
+const getToken = () => {
+  const token = localStorage.getItem("token");
+  return token ? `Bearer ${token}` : null;
+};
+
+const getHeaders = () => {
+  const token = getToken();
+
+  if (token) {
+    return {
+      Authorization: token,
+
+      "Content-Type": "multipart/form-data",
+    };
+  } else {
+    return {
+      "Content-Type": "multipart/form-data",
+    };
+  }
+};
+
 const API = axios.create({
     baseURL: BASI_API,
-})
+    headers: getHeaders(),
+});
 
-export { API }
+export { API };
