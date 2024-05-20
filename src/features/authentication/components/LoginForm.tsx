7268997ee291505fe40d1c0758d52cfe6c22455a
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-05-20 05:56:21 +0300

    finished login functionalities

--- src/features/authentication/components/LoginForm.tsx
+++ src/features/authentication/components/LoginForm.tsx
@@ -4,15 +4,37 @@ import Input from '../../../components/form/Input'
 import SubmitBtn from '../../../components/form/SubmitBtn';
 import { loginSchema, initialValues } from '../../../schemas/loginValidationSchema';
 import { useTranslation } from 'react-i18next';
+import { useLoginMutation } from '../../../services/queryHooks/useLoginQuery';
+import { getErrorWithResponse } from '../../../utils/apiError';
 
 const LoginForm: React.FC = () => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
+  const mutation = useLoginMutation();
+
+  const handleSubmit = async (values: any) => {
+    console.log(values)
+    try {
+      mutation.mutate(values);
+    } catch(err) {
+      console.log(err);
+    }
+  };
+
+  const errorWithResponse = getErrorWithResponse(mutation.error);
 
   return (
-    <Formik initialValues={initialValues} validationSchema={loginSchema} onSubmit={(values) => console.log(values)}>
+    <Formik initialValues={initialValues} validationSchema={loginSchema} onSubmit={handleSubmit}>
       {(formikProps) => {
+        console.log("is submitting", formikProps.isSubmitting)
         return (
           <Form className="max-w-[350px] flex flex-col gap-3">
+            {mutation.isError && (
+              <p className="text-error text-center">
+                {errorWithResponse
+                  ? errorWithResponse?.response?.data?.message
+                  : "An error occurred"}
+              </p>
+            )}
             <Input
               label={t("email")}
               name="email"
@@ -27,10 +49,7 @@ const LoginForm: React.FC = () => {
               type="password"
               placeholder="ahme*2laSlskdk"
             />
-            <SubmitBtn
-              disabled={formikProps.isSubmitting}
-              BtnTxt={t("signin")}
-            />
+            <SubmitBtn disabled={formikProps.isSubmitting} BtnTxt={t("signin")} />
           </Form>
         );
       }}
