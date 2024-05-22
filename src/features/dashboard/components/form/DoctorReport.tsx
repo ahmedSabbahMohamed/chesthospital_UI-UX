613dd68c10b881a2bd8f048b696fa6703785b751
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-05-22 17:57:37 +0300

    finished DoctorReport form

--- src/features/dashboard/components/form/DoctorReport.tsx
+++ src/features/dashboard/components/form/DoctorReport.tsx
@@ -1,25 +1,39 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Form, Formik } from 'formik'
 import Input from '../../../../components/form/Input'
 import SubmitBtn from '../../../../components/form/SubmitBtn'
+import { initialValues, validationSchema } from '../../../../schemas/doctorReportValidationSchema'
+import usePatientId from '../../../../hooks/PatientId'
+import useEmployeeId from '../../../../hooks/EmployeeId'
+import { usePostPatientReport } from '../../doctor/services/hooks/useDoctorQuery';
+import { idProps } from '../../../../utils/types'
 
-const DoctorReport: React.FC = () => {
+const DoctorReport: React.FC<idProps> = ({ id }) => {
 
-  const [loading, setLoading] = useState(false)
+  const patientId = usePatientId();
+  const employeeId = useEmployeeId();
+
+  const mutation = usePostPatientReport(id);
 
   const handleSubmit = async (values: any) => {
-    console.log("values", values)
+    values.patientId = patientId;
+    values.doctorId = employeeId;
+    try {
+      mutation.mutate(values);
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   return (
     <Formik
-      initialValues={{}}
-      validationSchema={""}
+      initialValues={initialValues}
+      validationSchema={validationSchema}
       onSubmit={handleSubmit}
     >
       {formikProps => {
         return (
-          <Form>
+          <Form className='flex items-end justify-center gap-2 flex-wrap'>
             <Input
               label='Diagnose illness'
               name='diagnose'
@@ -30,7 +44,7 @@ const DoctorReport: React.FC = () => {
             />
             <SubmitBtn
               BtnTxt='Add Report'
-              disabled={loading}
+              disabled={formikProps.isSubmitting}
             />
           </Form>
         )
