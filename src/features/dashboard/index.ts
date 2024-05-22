eda8fe00d3c24a90e9eef45f74a06a58185e967e
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-05-22 18:04:31 +0300

    imported shared components for entry point file

--- src/features/dashboard/index.ts
+++ src/features/dashboard/index.ts
@@ -1,5 +1,21 @@
 import Sidebar from "./components/ui/Sidebar";
 import SearchPatientForm from "./components/form/SearchPatientForm";
 import MedicalRecord from "./components/ui/MedicalRecord";
+import ConsultationRequest from "./components/form/ConsultationRequest";
+import DoctorReport from "./components/form/DoctorReport";
+import LabRequest from "./components/form/LabRequest";
+import MedicineRequest from "./components/form/MedicineRequest";
+import OxygenRequest from "./components/form/OxygenRequest";
+import RadiologyRequest from "./components/form/RadiologyRequest";
 
-export { Sidebar, SearchPatientForm, MedicalRecord };
+export {
+  Sidebar,
+  SearchPatientForm,
+  MedicalRecord,
+  ConsultationRequest,
+  DoctorReport,
+  LabRequest,
+  MedicineRequest,
+  OxygenRequest,
+  RadiologyRequest,
+};
