f501c8fac6bb7e969237c14cd1552141fb282981
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-05-22 18:00:12 +0300

    finished radioloy request form

--- src/features/dashboard/components/form/RadiologyRequest.tsx
+++ src/features/dashboard/components/form/RadiologyRequest.tsx
@@ -5,10 +5,28 @@ import {
   initialValues,
   validationSchema,
 } from "../../../../schemas/radiologyRequestSchema";
+import SubmitBtn from "../../../../components/form/SubmitBtn";
+import usePatientId from "../../../../hooks/PatientId";
+import useEmployeeId from "../../../../hooks/EmployeeId";
+import { useRadiologyRequest } from "../../doctor/services/hooks/useDoctorQuery";
+import { idProps } from "../../../../utils/types";
+import { toast } from "react-toastify";
 
-const RadiologyRequest: React.FC = () => {
-  const handleSubmit = (values: any) => {
-    console.log(values);
+const RadiologyRequest: React.FC<idProps> = ( { id } ) => {
+
+  const patientId = usePatientId();
+  const doctorId = useEmployeeId();
+
+  const mutation = useRadiologyRequest(id)
+
+  const handleSubmit = async (values: any) => {
+    values.patientId = parseInt(patientId);
+    values.doctorId = parseInt(doctorId);
+    try {
+      mutation.mutate(values);
+    } catch(err: any) {
+      toast.error(err);
+    }
   };
 
   return (
@@ -18,10 +36,13 @@ const RadiologyRequest: React.FC = () => {
         validationSchema={validationSchema}
         onSubmit={handleSubmit}
       >
-        <Form>
-          <Input name="name" label="Radiology Name:" />
-          <Input name="description" label="Description" />
-        </Form>
+        {(formikProps) => (
+          <Form className="flex items-end justify-center gap-2 flex-wrap">
+            <Input name="name" label="Radiology Name:" />
+            <Input name="description" label="Description" />
+            <SubmitBtn BtnTxt="Send Request" disabled={formikProps.isSubmitting} />
+          </Form>
+        )}
       </Formik>
     </div>
   );
