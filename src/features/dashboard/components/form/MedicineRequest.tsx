13f71ed9befd187a3b3d4e4104f0e18271b97a42
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-05-22 17:59:07 +0300

    finished medicine request form

--- src/features/dashboard/components/form/MedicineRequest.tsx
+++ src/features/dashboard/components/form/MedicineRequest.tsx
@@ -1,14 +1,31 @@
 import { Form, Formik } from "formik";
 import React from "react";
-import Input from "../../../../components/form/Input";
 import {
   initialValues,
   validationSchema,
 } from "../../../../schemas/medicineRequestSchema";
+import SubmitBtn from "../../../../components/form/SubmitBtn";
+import SelectInput from "../../../../components/form/SelectInput";
+import { useGetMedicine, useMedicineRequest } from "../../doctor/services/hooks/useDoctorQuery";
+import usePatientId from "../../../../hooks/PatientId";
+import useEmployeeId from "../../../../hooks/EmployeeId";
+import { idProps } from "../../../../utils/types";
+import { toast } from "react-toastify";
 
-const MedicineRequest: React.FC = () => {
-  const handleSubmit = (values: any) => {
-    console.log(values);
+const MedicineRequest: React.FC<idProps> = ({ id }) => {
+  const { data } = useGetMedicine();
+  const mutation = useMedicineRequest(id);
+  const patientId = usePatientId();
+  const doctorId = useEmployeeId();
+
+  const handleSubmit = async (values: any) => {
+    values.patientId = parseInt(patientId);
+    values.doctorId = parseInt(doctorId);
+    try {
+      mutation.mutate(values);
+    } catch(err: any) {
+      toast.error(err);
+    }
   };
 
   return (
@@ -18,9 +35,19 @@ const MedicineRequest: React.FC = () => {
         validationSchema={validationSchema}
         onSubmit={handleSubmit}
       >
-        <Form>
-          <Input name="medicine" label="Medicine" />
-        </Form>
+        {(formikProps) => (
+          <Form className="flex items-end justify-center gap-2 flex-wrap">
+            <SelectInput
+              options={data?.data.data || []}
+              name="medicine"
+              isMulti={true}
+            />
+            <SubmitBtn
+              BtnTxt="Send Request"
+              disabled={formikProps.isSubmitting}
+            />
+          </Form>
+        )}
       </Formik>
     </div>
   );
