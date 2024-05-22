c26f173664b2a0484eff952408112d8f947247f4
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-05-22 18:02:33 +0300

    enhanced Sidebar component

--- src/features/dashboard/components/ui/Sidebar.tsx
+++ src/features/dashboard/components/ui/Sidebar.tsx
@@ -2,8 +2,19 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { sidebarProps } from "../../../../utils/types";
 import { FaBars } from "react-icons/fa";
+import { AiOutlineLogout } from "react-icons/ai";
 
-const Sidebar: React.FC<sidebarProps> = ({ dir, sidebarContent, place = "ml-auto" }) => {
+const Sidebar: React.FC<sidebarProps> = ({
+  dir,
+  sidebarContent,
+  place = "ml-auto",
+  logoutText = "Logout"
+}) => {
+  const logout = () => {
+    localStorage.clear();
+    window.location.pathname = "/";
+  };
+
   return (
     <div dir={dir} className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
@@ -29,6 +40,12 @@ const Sidebar: React.FC<sidebarProps> = ({ dir, sidebarContent, place = "ml-auto
         <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
           {/* Sidebar content here */}
           {sidebarContent}
+          <li onClick={logout} className="text-error text-lg font-bold flex flex-row items-center gap-2 hover:bg-error hover:text-light hover:transition-ease cursor-pointer px-5 py-2 rounded-lg">
+            <span className="p-0 hover:bg-transparent">{logoutText}</span>
+            <span className="p-0 hover:bg-transparent">
+              <AiOutlineLogout />
+            </span>
+          </li>
         </ul>
       </div>
     </div>
