16a513808840a182448405e07a27d145966cc736
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-05-22 18:22:48 +0300

    finished consultation page for docotr

--- src/features/dashboard/doctor/pages/Consultation.tsx
+++ src/features/dashboard/doctor/pages/Consultation.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import ConsultationRequest from '../../components/form/ConsultationRequest';
 
 const Consultation: React.FC = () => {
   return (
-    <div>Consultation</div>
+    <div className="container flex flex-col justify-center items-center">
+      <h2 className='text-dark text-3xl font-bold mb-10'>Consultation Request</h2>
+      <ConsultationRequest />
+    </div>
   );
 };
 
