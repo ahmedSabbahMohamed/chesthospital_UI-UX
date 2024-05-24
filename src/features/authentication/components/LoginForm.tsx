daf36d0586166189c47dd5d1d6e59d7b710b0f8c
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-05-24 19:26:53 +0300

    refactored code for more organized, readable, scalable, and maintained component

--- src/features/authentication/components/LoginForm.tsx
+++ src/features/authentication/components/LoginForm.tsx
@@ -1,31 +1,37 @@
-import { Form, Formik } from 'formik'
-import Input from '../../../components/form/Input'
-import SubmitBtn from '../../../components/form/SubmitBtn';
-import { loginSchema, initialValues } from '../../../schemas/loginValidationSchema';
-import { useTranslation } from 'react-i18next';
-import { useLoginMutation } from '../../../services/queryHooks/useLoginQuery';
-import { getErrorWithResponse } from '../../../utils/apiError';
-import { useState } from 'react';
-import Loading from '../../../components/ui/Loading';
+import { Form, Formik, FormikHelpers } from "formik";
+import Input from "../../../components/form/Input";
+import SubmitBtn from "../../../components/form/SubmitBtn";
+import {
+  loginSchema,
+  initialValues,
+} from "../../../schemas/loginValidationSchema";
+import { useTranslation } from "react-i18next";
+import { useLoginMutation } from "../../../services/queryHooks/useLoginQuery";
+import { getErrorWithResponse } from "../../../utils/apiError";
+import { useState } from "react";
+import Loading from "../../../components/ui/Loading";
+import { LoginFormValues } from "../../../utils/types";
 
 const LoginForm: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(false);
   const { t } = useTranslation();
-  const mutation = useLoginMutation();
-  const errorWithResponse = getErrorWithResponse(mutation.error);
+  const loginMutation = useLoginMutation();
+  const errorWithResponse = getErrorWithResponse(loginMutation.error);
 
-  const handleSubmit = async (values: any) => {
+  const handleSubmit = async (
+    values: LoginFormValues,
+    { setSubmitting }: FormikHelpers<LoginFormValues>
+  ) => {
+    setLoading(true);
     try {
-      setLoading(true);
-      mutation.mutate(values);
-    } catch(err) {
-      console.log(err);
+      await loginMutation.mutateAsync(values);
+    } catch (error) {
+      console.error("Login failed", error);
     } finally {
       setLoading(false);
+      setSubmitting(false);
     }
   };
-
-  
 
   return (
     <>
@@ -34,35 +40,35 @@ const LoginForm: React.FC = () => {
         validationSchema={loginSchema}
         onSubmit={handleSubmit}
       >
-        {(formikProps) => {
-          console.log("is submitting", formikProps.isSubmitting);
-          return (
-            <Form className="max-w-[350px] flex flex-col gap-3">
-              {mutation.isError && (
-                <p className="text-error text-center">
-                  {errorWithResponse
-                    ? errorWithResponse?.response?.data?.message
-                    : "An error occurred"}
-                </p>
-              )}
-              <Input
-                label={t("email")}
-                name="email"
-                id="email"
-                type="email"
-                placeholder="ahmed@gamil.com"
-              />
-              <Input
-                label={t("password")}
-                name="password"
-                id="password"
-                type="password"
-                placeholder="ahme*2laSlskdk"
-              />
-              <SubmitBtn disabled={loading} BtnTxt={t("signin")} />
-            </Form>
-          );
-        }}
+        {({ isSubmitting }) => (
+          <Form className="max-w-[350px] flex flex-col gap-3">
+            {loginMutation.isError && (
+              <p className="text-error text-center">
+                {errorWithResponse
+                  ? errorWithResponse?.response?.data?.message
+                  : t("An error occurred")}
+              </p>
+            )}
+            <Input
+              label={t("email")}
+              name="email"
+              id="email"
+              type="email"
+              placeholder="ahmed@gamil.com"
+            />
+            <Input
+              label={t("password")}
+              name="password"
+              id="password"
+              type="password"
+              placeholder="ahme*2laSlskdk"
+            />
+            <SubmitBtn
+              disabled={isSubmitting || loading}
+              BtnTxt={t("signin")}
+            />
+          </Form>
+        )}
       </Formik>
       {loading && (
         <div className="w-full h-full bg-[#a2a2a2a3] grid place-items-center fixed z-[999999] top-0 left-0">
@@ -71,6 +77,6 @@ const LoginForm: React.FC = () => {
       )}
     </>
   );
-}
+};
 
-export default LoginForm
+export default LoginForm;
