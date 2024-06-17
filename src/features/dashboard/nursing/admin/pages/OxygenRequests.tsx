148b442bc8f6faa3f4eaf93d5a367b85377a68d4
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-06-17 19:51:41 +0300

    enhanced OxygenRequests page

--- src/features/dashboard/nursing/admin/pages/OxygenRequests.tsx
+++ src/features/dashboard/nursing/admin/pages/OxygenRequests.tsx
@@ -6,20 +6,44 @@ import {
 import { getErrorWithResponse } from "../../../../../utils/apiError";
 import { Case, Switch, Default } from "react-if";
 import Loading from "../../../../../components/ui/Loading";
+import Error from "../../../sharedPages/components/ui/Error";
+import { OxygenRequestRowProps } from "../../utils/types";
+import Table from "../../../sharedPages/components/ui/Table";
 
 const OxygenRequests: React.FC = () => {
+
   const { data, error, isLoading } = useGetOxygenRequests();
   const { mutateAsync } = useDeleteOxygenRequest();
-
-  const handleDeleteMedicineRequest = async (id: string) => {
+  const handleDelelteOxygenRequest = async (id: string) => {
     try {
       await mutateAsync(id);
     } catch (err) {
       console.log(err);
     }
   };
+  const message = getErrorWithResponse(error)?.response?.data.message;
 
-  const message = getErrorWithResponse(error);
+  const OxygenRequestRow: React.FC<OxygenRequestRowProps> = ({ index, request, onDelete }) => {
+    return (
+      <tr key={index}>
+        <th>{index + 1}</th>
+        <td>{request.patientId}</td>
+        <td className="flex flex-row flex-wrap gap-2">
+          <span className="bg-light rounded-md py-1 px-2">
+            {request.oxygenLevel}
+          </span>
+        </td>
+        <td>
+          <button
+            onClick={() => onDelete(request.id)}
+            className="rounded bg-error text-white font-bold py-2 px-4"
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+    );
+  }
 
   return (
     <div>
@@ -28,58 +52,27 @@ const OxygenRequests: React.FC = () => {
       </h2>
       <div className="mb-3">
         <Switch>
-          <Default>
-            <h3 className="text-error text-center">No Requests</h3>
-          </Default>
           <Case condition={isLoading}>
             <Loading />
           </Case>
           <Case condition={error !== undefined && error !== null}>
-            <h3 className="text-error text-center">
-              Error: {message?.response?.data.message}
-            </h3>
+            <Error message={message || "An error occurred"} />
           </Case>
           <Case condition={data?.data?.data?.requests.length > 0}>
-            <div className="overflow-x-auto">
-              <table className="table table-xs sm:table-sm md:table-lg bg-white overflow-hidden">
-                <thead className="bg-dark text-white">
-                  <tr>
-                    <th>ID</th>
-                    <th>Patient ID</th>
-                    <th>Level</th>
-                    <th>Options</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {data?.data?.data.requests.map(
-                    (request: any, index: number) => {
-                      return (
-                        <tr key={index}>
-                          <th>{index + 1}</th>
-                          <td>{request.patientId}</td>
-                          <td className="flex flex-row flex-wrap gap-2">
-                            <span className="bg-light rounded-md py-1 px-2">
-                              {request.oxygenLevel}
-                            </span>
-                          </td>
-                          <td>
-                            <button
-                              onClick={() =>
-                                handleDeleteMedicineRequest(request.id)
-                              }
-                              className="rounded bg-error text-white font-bold py-2 px-4"
-                            >
-                              Delete
-                            </button>
-                          </td>
-                        </tr>
-                      );
-                    }
-                  )}
-                </tbody>
-              </table>
-            </div>
+            <Table headers={["ID", "Patient ID", "Oxygen Level", "Options"]}>
+                  {data?.data?.data.requests.map((request: any, index: number) => (
+                    <OxygenRequestRow
+                      key={index}
+                      request={request}
+                      index={index}
+                      onDelete={handleDelelteOxygenRequest}
+                    />
+                  ))}
+            </Table>
           </Case>
+          <Default>
+            <h3 className="text-error text-center">No Requests</h3>
+          </Default>
         </Switch>
       </div>
     </div>
