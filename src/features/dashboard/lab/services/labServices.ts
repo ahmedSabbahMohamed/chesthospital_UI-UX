43dcf2d5b3936dcc4441021fd188c9632c8ba3d3
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-06-19 23:06:41 +0300

    defined lab services

--- src/features/dashboard/lab/services/labServices.ts
+++ src/features/dashboard/lab/services/labServices.ts
@@ -1,8 +1,8 @@
 import { API } from ".";
 import { LAB_ENDPOINTS } from "./labEndpoints";
 
-const postLabResults = () => {
-  return API.post(LAB_ENDPOINTS.POST_RESULTS);
+const postLabResults = (results: any) => {
+  return API.post(LAB_ENDPOINTS.POST_RESULTS, results);
 };
 
 export { postLabResults };
