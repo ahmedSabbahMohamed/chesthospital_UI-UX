0ba8eabf74ace15f9eb2dc55c61641ea3289e672
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-06-19 23:05:27 +0300

    completed ResultForm for lab

--- src/features/dashboard/lab/components/form/ResultForm.tsx
+++ src/features/dashboard/lab/components/form/ResultForm.tsx
@@ -3,15 +3,51 @@ import React from "react";
 import Input from "../../../../../components/form/Input";
 import FileInput from "../../../../../components/form/FileInput";
 import SubmitBtn from "../../../../../components/form/SubmitBtn";
+import {
+  initialValues,
+  validationSchema,
+} from "../../../../../schemas/addLabResultSchema";
+import useEmployeeId from "../../../../../hooks/EmployeeId";
+import { usePostLabResults } from "../../services/labQueries";
+import { convertToFormData } from "../../../../../utils/helpers";
 
 const ResultForm: React.FC = () => {
+  const { mutateAsync } = usePostLabResults();
+  const docotrId = useEmployeeId();
+  const handleSubmit = async (
+    values: any,
+    { setSubmitting }: { setSubmitting: (isSubmitting: boolean) => void }
+  ) => {
+    const labResults = {
+      ...values,
+      doctorId: docotrId,
+    };
+    try {
+      const formData = convertToFormData(labResults);
+      await mutateAsync(formData);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
   return (
-    <Formik initialValues={{}} onSubmit={(values) => {console.log(values)}}>
-        <Form>
-            <Input label="Patient ID" name="patientId" />
-            <FileInput />
-            <SubmitBtn BtnTxt="Send Results" disabled={false} />
-        </Form>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {({ isSubmitting }) => {
+        return (
+          <Form className="flex flex-col gap-2">
+            <Input label="Patient ID:" name="patientId" />
+            <Input label="Lab Name:" name="name" />
+            <FileInput name="result" />
+            <SubmitBtn BtnTxt="Send Results" disabled={isSubmitting} />
+          </Form>
+        );
+      }}
     </Formik>
   );
 };
