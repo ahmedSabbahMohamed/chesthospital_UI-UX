d56b2e66f0cc5656123b8468885e19b4b752e72d
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-06-20 13:02:51 +0300

    fixed code for correct deploy

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,20 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { Switch, Case, Default } from 'react-if';
+import React from 'react';
+// import { Switch, Case, Default } from 'react-if';
 import DoctorRoutes from './DoctorRoutes';
-import ReceptionRoutes from './ReceptionRoutes';
-import PublicRoutes from './PublicRoutes';
-import ManagerRoutes from './ManagerRoutes';
-import PharmacistRoutes from './PharmacistRoutes';
-import NursingAdminRoutes from './NursingAdminRoutes';
-import LabAdminRoutes from './LabAdminRoutes';
+// import ReceptionRoutes from './ReceptionRoutes';
+// import PublicRoutes from './PublicRoutes';
+// import ManagerRoutes from './ManagerRoutes';
+// import PharmacistRoutes from './PharmacistRoutes';
+// import NursingAdminRoutes from './NursingAdminRoutes';
+// import LabAdminRoutes from './LabAdminRoutes';
 
 const AppRoutes: React.FC = () => {
-  const [specialization, setSpecialization] = useState<string | null>(null);
+  // const [specialization, setSpecialization] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchedSpecialization = localStorage.getItem("specialization");
-    setSpecialization(fetchedSpecialization);
-    }, []);
+  // useEffect(() => {
+  //   const fetchedSpecialization = localStorage.getItem("specialization");
+  //   setSpecialization(fetchedSpecialization);
+  //   }, []);
 
   return (
     // <Switch>
